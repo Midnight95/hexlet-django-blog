89416466a65d74917e8c4c30e89026b82dc13a4c
midnight <ivanator666@gmail.com>
2023-10-17 19:53:14 +0300

    step 13

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 from django.views import View
 
+from hexlet_django_blog.article.models import Article
+
 
 class IndexView(View):
+
     def get(self, request):
+        articles = Article.objects.all()[:15]
         return render(
             request,
             'articles/index.html',
-            context={'name': 'hexlet_django_blog'}
+            context={
+                'name': 'hexlet_django_blog',
+                'articles': articles,
+                }
         )
 
 
